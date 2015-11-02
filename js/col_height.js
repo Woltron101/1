07573132df3c11139a6b3d1f3a196342856a6ef5
Sergey Khiblin <woltron101@gmail.com>
2015-11-02 21:17:11 +0200

    pixel_perfect

--- js/col_height.js
+++ js/col_height.js
@@ -33,37 +33,47 @@ $(document).ready(function() {
 	loop:true,
 	margin:0,
 	responsiveClass:true,
+	navigationText : false,
 	responsive:{
 		320: {
 			items:1,
-			nav: true
+			nav: true,
+			navigationText : false
 		},
 
 		540:{
 			items:2,
-			nav:true
+			nav:true,
+			navigationText : false
 		},
 		768:{
 			items:3,
-			nav:true
+			nav:true,
+			navigationText : false
 		},
 		992:{
 			items:4,
 			nav:true,
+			navigationText : false
 		}
 	}
 })
 	setEqualHeight($(".product-box .pr-bg"));
-	setTopMargin ('.center-row-header');
-	setTopMargin ('.row.sells-hd.tovar');
-	setTopMargin ('.article-comment');
-	setTopMargin ('.footer');
+//	setTopMargin ('.center-row-header');
+//	setTopMargin ('.row.sells-hd.tovar');
+//	setTopMargin ('.article-comment');
+//	setTopMargin ('.footer');
+			
+		
+		$('a.right').prepend($('a.left.carousel-control svg').clone());
+		$('.owl-next, .owl-prev').prepend($('a.left.carousel-control svg').clone());
+		
 
 });
 $(window).resize(function() {
 	setEqualHeight($(".product-box .pr-bg"));
-	setTopMargin ('.center-row-header');	
-	setTopMargin ('.row.sells-hd.tovar');
-	setTopMargin ('.article-comment');
-	setTopMargin ('.footer');
+//	setTopMargin ('.center-row-header');	
+//	setTopMargin ('.row.sells-hd.tovar');
+//	setTopMargin ('.article-comment');
+//	setTopMargin ('.footer');
 })
